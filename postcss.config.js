81574ce4797bac01ded24166445e398d33e89e82
Victor Yoalli <victor@vientodigital.com>
2020-01-29 19:04:32 -0600

    purgecss & nanocss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html']
+        }),
+        require('cssnano')({
+            preset: 'default',
+        })
     ]
 }

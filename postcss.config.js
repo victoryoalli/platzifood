cc393a51c4150536929d7e42c8bdeb191fa016a8
Victor Yoalli <victor@vientodigital.com>
2020-03-10 14:01:34 -0600

    options for PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,12 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
         purgecss({
-            content: ['./**/*.html']
+            content: ['./**/*.html'],
+            //Para agregar soporte para otro tipo de archivos.
+            //content: ['./**/*.jsx'],
+            //content: ['./**/*.vue'],
+            //Para soportar pseudo-clases
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
         }),
         require('cssnano')({
             preset: 'default',
